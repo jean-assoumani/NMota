7c48d7fe65b39188726e26f4db83d0229aeb82b8
Jean Assoumani <assoumani.jean.patrick.gmail.com>
2024-09-21 17:40:48 +0200

    Add single-photo.php

---
+++ single-photo.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<!-- 
+Affichage d'une page individuelle pour une photo avec ses informations, interactions et suggestions
+-->
+
+<main class="site-main single-photo">
+
+</main>
+
+<?php get_footer(); ?>
